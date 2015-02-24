4cd4a8306009f07d8643ba95931a1d7557b0c983
dleybovich <ariedov@gmail.com>
2015-02-23 23:13:01 -0800

    implemented new item dialog

--- js/data.js
+++ js/data.js
@@ -8,4 +8,38 @@ $(document).ready(function() {
 			alert("Product " +  name + " saved!");
 		});
 	};
+
+	$("#new").click(function() {
+		bootbox.dialog({
+                title: "Add a new item.",
+                message: 
+					"<form>"+
+					  "<div class='form-group'>" + 
+					    "<label for='name'>Product name</label>" +
+					    "<input type='name' class='form-control' id='name' placeholder='Product email'>" +
+					  "</div>" +
+					  "<div class='form-group'>" + 
+					    "<label for='price'>Price</label>" +
+					    "<input type='text' class='form-control' id='price' placeholder='Product price'>" +
+					  "</div>" +
+					  "<div class='form-group'>" +
+					    "<label for='image'>Image</label>" +
+					    "<input type='text' class='form-control' id='image' placeholder='Product image'>" +
+					  "</div>" +
+					"</form>",
+                buttons: {
+                    success: {
+                        label: "Save",
+                        className: "btn-success",
+                        callback: function () {
+                            var name = $('#name').val();
+                          	var image = $('#image').val();
+                          	var price = $('#price').val();
+                            alert("Hello " + name + ". You've chosen <b>" + name + "</b>");
+                        }
+                    }
+                }
+            }
+        );
+	});
 });
